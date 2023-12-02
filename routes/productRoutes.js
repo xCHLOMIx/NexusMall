54bc81f4c1afc02095589b21928b50449943c36e
xCHLOMIx <chlomi2006@gmail.com>
2023-12-02 09:51:43 +0200

    Noice

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,5 +7,6 @@ const upload = multer({ storage: storage });
 
 router.get('/record', productController.product_get)
 router.post('/save', upload.single('productImage'), productController.product_post)
+router.get('/', productController.product_display)
 
 module.exports = router;
